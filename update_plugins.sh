436e4a92aca303cb0932a0ea01090694078af888
Igor Guerrero <igorgue@protonmail.com>
2020-03-24 11:30:10 -0400

    Add cleanup

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Cleanup
+rm -rf bundle/vim-rust-doc/doc/tags
+
+# Checkout and stuff
 git submodule foreach git checkout master
 git submodule foreach git checkout -- .
 git submodule foreach git pull --ff-only
