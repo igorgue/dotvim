7c7333b5808ec6c74a46944e4f7c4575de247956
Igor Guerrero <igorgue@protonmail.com>
2020-06-24 19:03:11 -0400

    Update plugins and hold gitgutter

--- update_plugins.sh
+++ update_plugins.sh
@@ -9,6 +9,8 @@ git submodule foreach git checkout -- .
 git submodule foreach git pull --ff-only
 
 # Extra special modulesthat use develop as main branch
+cd bundle/vim-gitgutter && git reset --hard 5f6048da13ed8dee3bd5e453036e1f50e4ed7045
+cd -
 cd bundle/vim-python-mode && git checkout develop && git checkout -- . && git pull --ff-only
 cd -
 cd bundle/vim-powerline && git checkout develop && git checkout -- . && git pull --ff-only
