4d04ac7130bc01c029d1b15f7e1189a9a706ba2a
Igor Guerrero <igorgue@protonmail.com>
2020-06-24 19:01:45 -0400

    Fix script to update plugins

--- update_plugins.sh
+++ update_plugins.sh
@@ -10,4 +10,6 @@ git submodule foreach git pull --ff-only
 
 # Extra special modulesthat use develop as main branch
 cd bundle/vim-python-mode && git checkout develop && git checkout -- . && git pull --ff-only
+cd -
 cd bundle/vim-powerline && git checkout develop && git checkout -- . && git pull --ff-only
+cd -
